08f5ab7f85299ebe33f72eedbd2fa82d20373d74
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-07-08 20:15:10 +0100

    Added movie repository

---
+++ fs-blazor-2025-07-08/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace fsblazor20250708.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public double Rating { get; set; }
+    }
+}
